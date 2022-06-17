f2f484d1af90d3476327de31cee7895bc423e449
parinovK <parinovk@yandex.ru>
2022-06-17 11:33:50 +0300

    loading project
    add 2 .cpp files in repository.
    Test_Access_to_Memory.cpp - checking offsets & the main functionality of the cheat
    FindOffsets.cpp - the main functions for finding offsets

---
+++ FindOffsets.cpp
@@ -0,0 +1,104 @@
+//поиск оффсетов
+#include <vector>
+#include <windows.h>
+#include <filesystem>
+
+class OffsetsFinder
+{
+private:
+	BYTE* fileBytes = nullptr;
+
+	size_t fileSize = 0;
+	uint32_t codeOffset = 0;
+
+	uint32_t world_offset = 0;
+	uint32_t objects_offset = 0;
+	uint32_t names_offset = 0;
+	uint32_t pool_offset = 0;
+private:
+	bool CompareByteArray(BYTE* data, BYTE* sig, size_t size) {
+		for (size_t i = 0; i < size; i++)
+		{
+			if (data[i] != sig[i]) {
+				if (sig[i] == 0x00) continue;
+				return false;
+			}
+		}
+		return true;
+	}
+	size_t FindSignature(std::vector<BYTE> sig, size_t start_at = 0) {
+		for (size_t offset = start_at; offset < fileSize - sig.size(); offset++) {
+			bool equals = CompareByteArray(reinterpret_cast<BYTE*>(fileBytes + offset), sig.data(), sig.size());
+			if (equals) {
+				return offset;
+			}
+
+		}
+		return 0;
+	}
+	uint32_t FindOffset(std::vector<BYTE>* sigv, uint32_t size, size_t start_at = 0) {
+		for (auto i = 0; i < size; i++) {
+			auto sig = sigv[i];
+			auto offset = FindSignature(sig, start_at);
+			if (!offset) continue;
+			auto k = 0;
+			for (; sig[k]; k++);
+			uint32_t nextOffset = *reinterpret_cast<uint32_t*>(fileBytes + offset + k);
+			return offset + k + 4 + nextOffset + codeOffset;
+		}
+		return 0;
+	}
+public:
+	~OffsetsFinder() {
+		if (fileBytes) {
+			delete[] fileBytes;
+		}
+	}
+	size_t LoadFile(const char* path) {
+		auto file = CreateFileA(path, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+		if (file != INVALID_HANDLE_VALUE) {
+			LARGE_INTEGER size;
+			GetFileSizeEx(file, &size);
+			fileSize = size.QuadPart;
+			fileBytes = new BYTE[fileSize];
+			DWORD readed;
+			if (!ReadFile(file, fileBytes, fileSize, &readed, 0) || readed != fileSize) {
+				return 0;
+			};
+			auto new_header_ptr = reinterpret_cast<PIMAGE_DOS_HEADER>(fileBytes)->e_lfanew;
+			auto baseOfCode = reinterpret_cast<PIMAGE_NT_HEADERS>(fileBytes + new_header_ptr)->OptionalHeader.BaseOfCode;
+			auto rawDataPtr = reinterpret_cast<PIMAGE_SECTION_HEADER>(fileBytes + new_header_ptr + sizeof(IMAGE_NT_HEADERS))->PointerToRawData;
+			codeOffset = baseOfCode - rawDataPtr;
+			CloseHandle(file);
+		}
+		return fileSize;
+	};
+	uint32_t GetGWorld() {
+		if (!world_offset) {
+			static std::vector<BYTE> sigv[] = { { 0x48, 0x8B, 0x1D, 0x00, 0x00, 0x00, 0x00, 0x48, 0x85, 0xDB, 0x74, 0x00, 0x41, 0xB0, 0x01 }, { 0x48, 0x8b, 0x05, 0x00, 0x00, 0x00, 0x00, 0x75, 0x1b }, {0x48, 0x8B, 0x05, 0x00, 0x00, 0x00, 0x00, 0x48, 0x8B, 0x88, 0x00, 0x00, 0x00, 0x00, 0x48, 0x85, 0xC9, 0x74, 0x06, 0x48, 0x8B, 0x49, 0x70} };
+			world_offset = FindOffset(sigv, 3, 0x1000);
+		}
+		return world_offset;
+	}
+	uint32_t GetGObjects() {
+		if (!objects_offset) {
+			static std::vector<BYTE> sigv[] = { {0x48, 0x8B, 0x05, 0x00, 0x00, 0x00, 0x00, 0x48, 0x8B, 0x0C, 0xC8, 0x48, 0x8D, 0x04, 0xD1, 0xEB}, {0x48 , 0x8b , 0x0d , 0x00 , 0x00 , 0x00 , 0x00 , 0x81 , 0x4c , 0xd1 , 0x08 , 0x00 , 0x00 , 0x00 , 0x40} };
+			objects_offset = FindOffset(sigv, 2, 0x1000);
+		}
+		return objects_offset;
+	}
+	uint32_t GetGNames() {
+		if (!names_offset) {
+			static std::vector<BYTE> sigv[] = { { 0x48, 0x8B, 0x05, 0x00, 0x00, 0x00, 0x00, 0x48, 0x85, 0xC0, 0x75, 0x5F }, { 0x48, 0x8D, 0x15, 0x00, 0x00, 0x00, 0x00, 0xEB, 0x00, 0x48, 0x8D, 0x0D }, {0x48, 0x8b, 0x3d, 0x00, 0x00, 0x00, 0x00, 0x48, 0x85, 0xff, 0x75, 0x3c} };
+			names_offset = FindOffset(sigv, 3, 0x1000);
+		}
+		return names_offset;
+	}
+	uint32_t GetNamePool() {
+		if (!pool_offset) {
+			static std::vector<BYTE> sigv[] = { { 0x48, 0x8d, 0x35, 0x00, 0x00, 0x00, 0x00, 0xeb, 0x16 } };
+			pool_offset = FindOffset(sigv, 1, 0x1000);
+		}
+		return pool_offset;
+	}
+};
---
+++ Test_Access_to_Memory.cpp
@@ -0,0 +1,153 @@
+﻿// Test_Access_to_Memory.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
+#include <iostream>
+#include "windows.h"
+#include "tlhelp32.h"
+#include "FindOffsets.cpp"
+#include <psapi.h>
+
+DWORD ProcessID;
+HANDLE handle;
+std::string version = "0.1b1";
+
+int Search_Process();
+int cheating_BP(int quantity_BPs, uint32_t UWorld_Offset = 0x3A3A4B0);
+void NoExistFile();
+void invalidOffsets();
+void validOffsets(uint32_t* Array_offsets);
+void ExistFile(OffsetsFinder* FindOffset);
+void OK_operation();
+
+int main(int argc, const char* argv[])
+{
+    setlocale(0, "");
+    int sel;
+
+    OffsetsFinder* FindOffset = new OffsetsFinder;
+    if (!FindOffset->LoadFile(argv[1]))
+        sel = 0;
+    else sel = 1;
+
+    switch (sel){
+        case 0:
+            NoExistFile();
+            break;
+        case 1:
+            ExistFile(FindOffset);
+            break;
+    }
+    return 0;
+}
+
+void NoExistFile()
+{
+    std::cout << "Pls run the game.\nWait...\n";
+    while (Search_Process() != 0)//while window not found
+    {
+        Sleep(5000);
+    }
+    std::cout << "Game is found..\n";
+    std::cout << "Input file not found. Offset cannot be found\nUsing standart offsets\n";
+    std::cout << "Bloodpoints modifier v" << version << "\nInput BPs quantity: "; int quantity_BPs; std::cin >> quantity_BPs;
+    if (cheating_BP(quantity_BPs) == 0)
+    {
+        OK_operation();
+    }
+    else std::cout << "invalid Offset\n";
+    Sleep(4000);
+}
+
+void ExistFile(OffsetsFinder* FindOffset)
+{
+    std::cout << "Input file is found.\nSearching for Offsets..\n";
+    const int n = 4;
+    int k = 0;
+    uint32_t Array_offsets[n] = { FindOffset->GetGWorld(), FindOffset->GetGObjects(), FindOffset->GetGNames(), FindOffset->GetNamePool() };
+    for (int i = 0; i < n; i++)
+        if (!Array_offsets[i]) k++;
+    if (k == n)//если все оффсеты = 0, значит устарели адреса, либо не тот экзешник. Будем использовать встроенные оффсеты
+        invalidOffsets();
+    else {
+        validOffsets(Array_offsets);
+        delete FindOffset;
+    }
+}
+
+void invalidOffsets()
+{
+    std::cout << "Pls run the game.\nWait...\n";
+    while (Search_Process() != 0)
+    {
+        Sleep(5000);
+    }
+    std::cout << "Game is found..\n";
+    std::cout << "Offsets cannot be found.\nCheck the name of the input file \"Dead-by-Daylight-Win64-Shipping.exe\"\nUsing standart offsets\n";
+    std::cout << "Bloodpoints modifier v" << version << "\nInput BPs quantity: "; int quantity_BPs; std::cin >> quantity_BPs;
+    if (cheating_BP(quantity_BPs) == 0)
+    {
+        OK_operation();
+    }
+    else std::cout << "invalid Offset\n";
+    Sleep(5000);
+}
+
+void validOffsets(uint32_t* Array_offsets)
+{
+    std::cout << "Offset adress List:\n";
+    printf("%-17s|\t%.7X\n", "UWorld", Array_offsets[0]);
+    printf("%-17s|\t%.7X\n", "TUObjectArray", Array_offsets[1]);
+    printf("%-17s|\t%.7X\n", "TNameEntryArray", Array_offsets[2]);
+    printf("%-17s|\t%.7X\n", "FNamePool", Array_offsets[3]);
+
+    std::cout << "Pls run the game.\nWait...\n";
+    while (Search_Process() != 0)//пока окно не найдено
+    {
+        Sleep(5000);
+    }
+    std::cout << "Game is found..\n";
+
+    std::cout << "Bloodpoints modifier v" << version << "\nInput BPs quantity: "; int quantity_BPs; std::cin >> quantity_BPs;
+    if (cheating_BP(quantity_BPs, Array_offsets[0]) == 0)
+    {
+        OK_operation();
+    }
+    else std::cout << "invalid Offset\n";
+    Sleep(5000);
+}
+
+void OK_operation()
+{
+    std::cout << "\nOK";
+    Beep(523, 100);
+    Sleep(3000);
+}
+  
+int Search_Process()
+{
+    HWND hwnd = FindWindowA("UnrealWindow", NULL);
+
+    if (hwnd == NULL)
+        return -1;
+    else
+    {
+        GetWindowThreadProcessId(hwnd, &ProcessID);
+        handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessID);
+
+        if (ProcessID == NULL) return -1;
+        else return 0;
+    }
+}
+
+int cheating_BP(int quantity_BPs, uint32_t UWorld_offset)
+{
+        float Read = 0; // Read value to memory
+        uint32_t offsetBP = 0x6EF8DA08;//offset
+        uint32_t offsetAdress = UWorld_offset + offsetBP;//Base+offset
+        ReadProcessMemory(handle, (PBYTE*)offsetAdress, &Read, sizeof(Read), 0); //Check valid read
+        if (Read == 0)//if Read=0 then invalid offset
+            return -1;
+        else {//ok
+            WriteProcessMemory(handle, (LPVOID)offsetAdress, &quantity_BPs, sizeof(quantity_BPs), 0);//write value Read to memory
+            return 0;
+        }
+}
