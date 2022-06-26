43e644ec3d5894cfa7ca1bbf6f65b0d66749d1dd
parinovK <62334658+parinovK@users.noreply.github.com>
2022-06-26 14:53:48 +0300

    update .cpp
    unnecessary methods have been removed, an additional class "Process" has been added

--- Test_Access_to_Memory.cpp
+++ Test_Access_to_Memory.cpp
@@ -6,43 +6,65 @@
 #include "FindOffsets.cpp"
 #include <psapi.h>
 
-DWORD ProcessID;
-HANDLE handle;
+class Process {
+private:
+    DWORD ProcessID;
+    HANDLE handle;
+public:
+    int Search_Process(LPCSTR WindowName = "UnrealWindow")
+    {
+      HWND hwnd = FindWindowA(WindowName, NULL);
+      if (hwnd == NULL)
+          return -1;
+      else
+         {
+           ProcessID = GetWindowThreadProcessId(hwnd, &ProcessID);
+           handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessID);
+
+           if (ProcessID == NULL) return -1;
+           else return 0;
+         }
+     }
+    DWORD get_ProcessID() { return this->ProcessID; }
+    HANDLE get_handle() { return this->handle; }
+};
+
+Process process;
 std::string version = "0.1b1";
 
-int Search_Process();
 int cheating_BP(int quantity_BPs, uint32_t UWorld_Offset = 0x3A3A4B0);
-void NoExistFile();
-void invalidOffsets();
-void validOffsets(uint32_t* Array_offsets);
-void ExistFile(OffsetsFinder* FindOffset);
-void OK_operation();
+int NoExistFile();
+int validOffsets(uint32_t* Array_offsets);
+int ExistFile(OffsetsFinder* FindOffset);
 
 int main(int argc, const char* argv[])
 {
     setlocale(0, "");
-    int sel;
-
     OffsetsFinder* FindOffset = new OffsetsFinder;
-    if (!FindOffset->LoadFile(argv[1]))
-        sel = 0;
-    else sel = 1;
 
-    switch (sel){
-        case 0:
-            NoExistFile();
-            break;
-        case 1:
-            ExistFile(FindOffset);
-            break;
+    if (!FindOffset->LoadFile(argv[1])) {
+        if (NoExistFile() == 0) {
+            std::cout << "\nOK";
+            Beep(523, 100);
+        }
+        else std::cout << "invalid Offset\n";
     }
+    else {
+        if (ExistFile(FindOffset) == 0) {
+            std::cout << "\nOK";
+            Beep(523, 100);
+        }
+        else std::cout << "invalid Offset\n";
+    }
+    Sleep(3000);
     return 0;
 }
 
-void NoExistFile()
+int NoExistFile()
 {
     std::cout << "Pls run the game.\nWait...\n";
-    while (Search_Process() != 0)//while window not found
+
+    while (process.Search_Process() != 0)//while window not found
     {
         Sleep(5000);
     }
@@ -50,48 +72,21 @@ void NoExistFile()
     std::cout << "Input file not found. Offset cannot be found\nUsing standart offsets\n";
     std::cout << "Bloodpoints modifier v" << version << "\nInput BPs quantity: "; int quantity_BPs; std::cin >> quantity_BPs;
     if (cheating_BP(quantity_BPs) == 0)
-    {
-        OK_operation();
-    }
-    else std::cout << "invalid Offset\n";
-    Sleep(4000);
+        return 0;
+    else return -1;
 }
 
-void ExistFile(OffsetsFinder* FindOffset)
+int ExistFile(OffsetsFinder* FindOffset)
 {
     std::cout << "Input file is found.\nSearching for Offsets..\n";
-    const int n = 4;
-    int k = 0;
-    uint32_t Array_offsets[n] = { FindOffset->GetGWorld(), FindOffset->GetGObjects(), FindOffset->GetGNames(), FindOffset->GetNamePool() };
-    for (int i = 0; i < n; i++)
-        if (!Array_offsets[i]) k++;
-    if (k == n)//если все оффсеты = 0, значит устарели адреса, либо не тот экзешник. Будем использовать встроенные оффсеты
-        invalidOffsets();
-    else {
-        validOffsets(Array_offsets);
-        delete FindOffset;
-    }
+    uint32_t Array_offsets[4] = { FindOffset->GetGWorld(), FindOffset->GetGObjects(), FindOffset->GetGNames(), FindOffset->GetNamePool() };
+    int ValidateOffsets = validOffsets(Array_offsets);
+    delete FindOffset;
+    if (ValidateOffsets == 0) return 0;
+    else return -1;
 }
 
-void invalidOffsets()
-{
-    std::cout << "Pls run the game.\nWait...\n";
-    while (Search_Process() != 0)
-    {
-        Sleep(5000);
-    }
-    std::cout << "Game is found..\n";
-    std::cout << "Offsets cannot be found.\nCheck the name of the input file \"Dead-by-Daylight-Win64-Shipping.exe\"\nUsing standart offsets\n";
-    std::cout << "Bloodpoints modifier v" << version << "\nInput BPs quantity: "; int quantity_BPs; std::cin >> quantity_BPs;
-    if (cheating_BP(quantity_BPs) == 0)
-    {
-        OK_operation();
-    }
-    else std::cout << "invalid Offset\n";
-    Sleep(5000);
-}
-
-void validOffsets(uint32_t* Array_offsets)
+int validOffsets(uint32_t* Array_offsets)
 {
     std::cout << "Offset adress List:\n";
     printf("%-17s|\t%.7X\n", "UWorld", Array_offsets[0]);
@@ -100,7 +95,7 @@ void validOffsets(uint32_t* Array_offsets)
     printf("%-17s|\t%.7X\n", "FNamePool", Array_offsets[3]);
 
     std::cout << "Pls run the game.\nWait...\n";
-    while (Search_Process() != 0)//пока окно не найдено
+    while (process.Search_Process() != 0)//пока окно не найдено
     {
         Sleep(5000);
     }
@@ -108,46 +103,22 @@ void validOffsets(uint32_t* Array_offsets)
 
     std::cout << "Bloodpoints modifier v" << version << "\nInput BPs quantity: "; int quantity_BPs; std::cin >> quantity_BPs;
     if (cheating_BP(quantity_BPs, Array_offsets[0]) == 0)
-    {
-        OK_operation();
-    }
-    else std::cout << "invalid Offset\n";
-    Sleep(5000);
-}
-
-void OK_operation()
-{
-    std::cout << "\nOK";
-    Beep(523, 100);
-    Sleep(3000);
+        return 0;
+    else return -1;
 }
   
-int Search_Process()
-{
-    HWND hwnd = FindWindowA("UnrealWindow", NULL);
 
-    if (hwnd == NULL)
-        return -1;
-    else
-    {
-        GetWindowThreadProcessId(hwnd, &ProcessID);
-        handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessID);
-
-        if (ProcessID == NULL) return -1;
-        else return 0;
-    }
-}
 
 int cheating_BP(int quantity_BPs, uint32_t UWorld_offset)
-{
+{ 
         float Read = 0; // Read value to memory
         uint32_t offsetBP = 0x6EF8DA08;//offset
         uint32_t offsetAdress = UWorld_offset + offsetBP;//Base+offset
-        ReadProcessMemory(handle, (PBYTE*)offsetAdress, &Read, sizeof(Read), 0); //Check valid read
+        ReadProcessMemory(process.get_handle(), (PBYTE*)offsetAdress, &Read, sizeof(Read), 0); //Check valid read
         if (Read == 0)//if Read=0 then invalid offset
             return -1;
         else {//ok
-            WriteProcessMemory(handle, (LPVOID)offsetAdress, &quantity_BPs, sizeof(quantity_BPs), 0);//write value Read to memory
+            WriteProcessMemory(process.get_handle(), (LPVOID)offsetAdress, &quantity_BPs, sizeof(quantity_BPs), 0);//write value Read to memory
             return 0;
         }
 }
